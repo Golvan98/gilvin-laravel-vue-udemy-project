04d07b791ac59d658badcf0517a918ac68c3a955
Golvan98 <gilvinsz@gmail.com>
2023-02-22 07:25:06 -0500

    modified middleware->inertiahandle request feb 22

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -17,8 +17,8 @@ import { Link } from '@inertiajs/vue3'
 import { ref } from 'vue'
 
 
-const timer = ref(0)
-setInterval(() => timer.value++, 1000)
+/* const timer = ref(0)
+setInterval(() => timer.value++, 1000) */
 
 </script>
 
