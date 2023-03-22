693fd5c5b3ff3c0ffeff9a84629df6dfcd060e76
Golvan98 <gilvinsz@gmail.com>
2023-03-22 06:46:09 -0400

    directives for reptitive classes March 22

--- resources/js/Pages/Listing/Create.vue
+++ resources/js/Pages/Listing/Create.vue
@@ -2,71 +2,71 @@
    <form @submit.prevent="create">
   <div class="grid grid-cols-6 gap-4">
     <div class="col-span-2">
-      <label class="block mb-1 text-gray-500 dark:text-gray-300">Beds</label>
-      <input v-model.number="form.beds" type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500"/>
+      <label class="label">Beds</label>
+      <input v-model.number="form.beds" type="text" class="input"/>
       <div v-if="errors.beds">
         {{  errors.beds }}
       </div>
     </div>
 
     <div class="col-span-2">
-      <label class="block mb-1 text-gray-500 dark:text-gray-300"> Baths</label>
-      <input v-model.number="form.baths" type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500"/>
+      <label class="label"> Baths</label>
+      <input v-model.number="form.baths" type="text" class="input"/>
       <div v-if="errors.baths">
         {{  errors.baths }}
       </div>
     </div>
 
     <div class="col-span-2">
-      <label class="block mb-1 text-gray-500 dark:text-gray-300"> Area </label>
-      <input v-model.number="form.area" type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500"/>
+      <label class="label"> Area </label>
+      <input v-model.number="form.area" type="text" class="input"/>
       <div v-if="errors.area">
         {{  errors.area }}
       </div>
     </div>
 
     <div class="col-span-4">
-      <label class="block mb-1 text-gray-500 dark:text-gray-300"> City</label>
-      <input v-model="form.city" type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500"/>
+      <label class="label"> City</label>
+      <input v-model="form.city" type="text" class="input"/>
       <div v-if="errors.city">
         {{  errors.city }}
       </div>
     </div>
 
     <div class="col-span-2">
-      <label class="block mb-1 text-gray-500 dark:text-gray-300"> Post Code</label>
-      <input v-model="form.code" type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500"/>
+      <label class="label"> Post Code</label>
+      <input v-model="form.code" type="text" class="input"/>
       <div v-if="errors.code">
         {{  errors.code }}
       </div>
     </div>
 
     <div class="col-span-4">
-      <label class="block mb-1 text-gray-500 dark:text-gray-300"> Street</label>
-      <input v-model="form.street" type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500"/>
+      <label class="label"> Street</label>
+      <input v-model="form.street" type="text" class="input"/>
       <div v-if="errors.street">
         {{  errors.street }}
       </div>
     </div>
 
     <div class="col-span-2">
-      <label class="block mb-1 text-gray-500 dark:text-gray-300"> street_nr</label>
-      <input v-model.number="form.street_nr" type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500"/>
+      <label class="label"> street_nr</label>
+      <input v-model.number="form.street_nr" type="text" class="input"/>
       <div v-if="errors.street_nr">
         {{  errors.street_nr }}
       </div>
     </div>
 
     <div class="col-span-6">
-      <label class="block mb-1 text-gray-500 dark:text-gray-300"> Price </label>
-      <input v-model.number="form.price" type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500"/>
+      <label class="label"> Price </label>
+      <input v-model.number="form.price" type="text" class="input"/>
       <div v-if="errors.price">
         {{  errors.price }}
       </div>
     </div>
 
     <div class="col-span-6">
-      <button type="submit" class="bg-indigo-600 hover:bg-indigo-500 rounded-md text-white p-2 font-bold" >Create</button>
+      <button type="submit" class="primary-btn" >Create</button>
     </div>
   </div>
 </form>
