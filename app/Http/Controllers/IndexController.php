4e512b25989a6d69d272b5a0290a8e3964c53e0b
Golvan98 <gilvinsz@gmail.com>
2023-03-24 08:08:23 -0400

    added login with Authentication logic in Auth Controller March 24

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Models\Listing;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,7 +10,7 @@ class IndexController extends Controller
     
     public function index()
     {
-        
+    dd(Auth::user());  
         return inertia('Index/Index',
 
                         [ 
