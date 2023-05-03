557dadc1c2d876ad00e73dcb8419212a0a115c8a
Golvan98 <gilvinsz@gmail.com>
2023-05-03 06:50:51 -0400

    added route prefix for newly made RealtorController May 05

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
+use App\Http\Controllers\RealtorListingController;
 use App\Http\Controllers\UserAccountController;
 use Illuminate\Support\Facades\Route;
 
@@ -33,3 +34,11 @@ Route::post('login', [AuthController::class, 'store'])->name('login.store');
 Route::delete('logout', [AuthController::class, 'destroy'])->name('logout');
 
 Route::resource('user-account', UserAccountController::class)->except('destroy');
+
+
+Route::prefix('realtor')
+->name('realtor.')
+->middleware('auth')
+->group(function () {
+    Route::resource('listing', RealtorListingController::class);
+});
