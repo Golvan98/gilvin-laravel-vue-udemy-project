a8f52da00ee5a92961607f8bc8e71e6b164034b3
Golvan98 <gilvinsz@gmail.com>
2023-05-11 09:47:22 -0400

    added ListingImage Model and migration for file upload functionality May 11

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Builder;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 
@@ -64,9 +65,10 @@ class Listing extends Model
         );
     }
 
-
-
-}
+    public function images(): HasMany
+    {
+        return $this->hasMany(ListingImage::class);
+    }
 
    
 
