48813692586e099c2186f168a9ec43ddcbbe96cb
Golvan98 <gilvinsz@gmail.com>
2023-05-14 07:41:39 -0400

    added progress bar but it doesn't work in my pc since it has been showing without using this code

--- resources/js/Pages/Realtor/ListingImage/Create.vue
+++ resources/js/Pages/Realtor/ListingImage/Create.vue
@@ -4,7 +4,7 @@
         <form @submit.prevent="upload">
             <section class="flex items-center gap-2 my-4">
                 <input class="border rounded-md file:px-4 file:py-2 border-gray-200 dark:border-gray-700 file:text-gray-700 fie:dark:text-gray-400 file:border-0 file:bg-gray-100 dark:bg-gray-800 file:font-medium file:hover:bg-gray-200 file:dark:hover:bg-gray-700 file:hover:cursor-pointer file:mr-4"
-                   type="file" multiple @input="addFiles" />
+                type="file" multiple @input="addFiles" />
                 <button type="submit" :disabled="!canUpload" class="btn-outline disabled:opacity-25 disabled:cursor-not-allowed">Upload</button>
                 <button type="reset" class="btn-outline" @click="reset">Reset</button>
             </section>
@@ -18,8 +18,19 @@
   import { computed } from 'vue'
   import Box from '@/Components/UI/Box.vue'
   import { useForm } from '@inertiajs/inertia-vue3'
+  import { Inertia } from '@inertiajs/inertia'
+  import NProgress from 'nprogress'
 
 const props = defineProps({ listing: Object })
+
+Inertia.on('progress', (event) => 
+{
+    if (event.detail.progress.percentage)
+    {
+        NProgress.set((event.detail.progress.percentage / 100) * 0.4)
+    }
+})
+
 const form = useForm({
   images: [],
 })
